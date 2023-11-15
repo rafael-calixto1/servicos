f09201b9e3c8bde6f2276460374b275626f7e5d8
calixtorafael-1 <calixxtorafaell@gmail.com>
2023-11-15 16:30:11 -0300

    add entity.Servico entity

---
+++ back-servico/src/main/java/com/servico/backservico/entity/Servico.java
@@ -0,0 +1,26 @@
+package com.servico.backservico.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "servico")
+@Data
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Temporal(TemporalType.DATE)
+    private Date dataInicio = new Date();
+    @Temporal(TemporalType.DATE)
+    private Date dataTermino = new Date();
+    private String descricaoServico;
+    private Double valorServico;
+    private Double valorPago;
+    @Temporal(TemporalType.DATE)
+    private Date dataPagamento;
+    private String status;
+}
