16ad7a45462505c2a62e37cfc962bd24d299d638
calixtorafael-1 <calixxtorafaell@gmail.com>
2023-11-20 19:41:02 -0300

    Fix: Resolve issue with validation annotations due to missing dependencies

--- back-servico/src/main/java/com/servico/backservico/controller/ServicoController.java
+++ back-servico/src/main/java/com/servico/backservico/controller/ServicoController.java
@@ -4,43 +4,47 @@ import com.servico.backservico.entity.Servico;
 import com.servico.backservico.service.ServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/servico")
+@Validated
 public class ServicoController {
 
     @Autowired
     private ServicoService servicoService;
+
     @GetMapping("/")
     public List<Servico> buscarTodos(){
         return servicoService.buscarTodos();
     }
+
     @GetMapping("/pagamentoPendente")
     public List<Servico> buscarServicosPagamentoPendente(){
         return servicoService.buscarServicosPagamentoPendente();
     }
+
     @GetMapping("/cancelados")
     public List<Servico> buscarServicosCancelados(){
         return servicoService.buscarServicosCancelados();
     }
+
     @PostMapping("/")
-    public Servico inserir(@RequestBody Servico servico){
+    public Servico inserir(@Validated @RequestBody Servico servico){
         return servicoService.inserir(servico);
     }
 
     @PutMapping("/")
-    public Servico alterar(@RequestBody Servico servico){
+    public Servico alterar(@Validated @RequestBody Servico servico){
         return servicoService.alterar(servico);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathVariable("id")Long id){
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
         servicoService.excluir(id);
         return ResponseEntity.ok().build();
     }
-
 }
